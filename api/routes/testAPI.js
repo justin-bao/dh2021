2c70493a0d2e8d8fa82134f0dba93feb83bb6cf9
justin-bao <justin.bao91@gmail.com>
2021-10-23 16:57:12 -0700

    got test for express to work

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('API is working properly');
+
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+
+router.get('/', cors(corsOptions), function(req, res, next) {
+  res.send('testAPI is working properly');
 });
 
 module.exports = router;
