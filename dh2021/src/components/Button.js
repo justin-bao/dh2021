3ff5bf1ad81006cebfac816c7632f72e0a8a2517
Melody Zhu <melodyyzhu@gmail.com>
2021-10-23 15:49:57 -0700

    set up router

---
+++ dh2021/src/components/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "react-router-dom";
+
+function Button(props) {
+  return (
+    <div>
+      <Link to={props.path}>
+        <button 
+          className={props.buttonType} 
+        >
+          {props.text}
+        </button>
+      </Link>
+    </div>
+  );
+}
+
+export default Button;
